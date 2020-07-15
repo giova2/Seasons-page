1a82b0b264b0208256907020f963a4d3f929e966
franco giovannangelo <fcgiova@gmail.com>
2020-07-15 16:24:32 -0300

    Updates

---
+++ precache-manifest.211cc64e0cdf7a6ab327e2920556743b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "58a367d98fefa1b966e0",
+    "url": "/static/css/main.d9ec465b.chunk.css"
+  },
+  {
+    "revision": "58a367d98fefa1b966e0",
+    "url": "/static/js/main.7f409f64.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "aad82d75cec72a1705744fe08cf1a551",
+    "url": "/index.html"
+  }
+];
